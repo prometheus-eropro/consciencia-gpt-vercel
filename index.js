7b2eca584e1e37ab5e5bcf62e139db58c230061e
prometheus-eropro <assistentes.prometheus@gmail.com>
2025-06-19 15:11:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const app = require("./api");
+const app = require("./api.js");
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
